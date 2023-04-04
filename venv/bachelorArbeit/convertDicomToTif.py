cc5237735238fa7980470b5ed9331454a181993b
nicole <nicolekozhuharova@gmail.com>
2023-04-04 21:53:25 +0300

    feat: added dicom to tif conversion

---
+++ venv/bachelorArbeit/convertDicomToTif.py
@@ -0,0 +1,20 @@
+import pydicom
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load DICOM image
+dcm_image = pydicom.dcmread('./images/dicomImages/1-15.dcm')
+
+# Convert DICOM to Pillow image
+pil_image = Image.fromarray(dcm_image.pixel_array)
+
+# Convert Pillow image to NumPy array
+np_image = np.array(pil_image)
+
+# Save image as TIFF
+plt.imsave('./images/jpegImages/1-15.tiff', np_image, cmap='gray')
+
+# Display the TIFF image using matplotlib
+plt.imshow(np_image, cmap='gray')
+plt.show()
