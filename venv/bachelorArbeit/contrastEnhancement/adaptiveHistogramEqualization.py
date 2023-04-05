04ca8b26871dee39e6bb8a0a51628ee08828f876
nicole <nicolekozhuharova@gmail.com>
2023-04-05 20:15:53 +0300

    feat: added function to display original and processed image

--- venv/bachelorArbeit/contrastEnhancement/adaptiveHistogramEqualization.py
+++ venv/bachelorArbeit/contrastEnhancement/adaptiveHistogramEqualization.py
@@ -1,5 +1,6 @@
 from skimage import io, exposure
 import matplotlib.pyplot as plt
+from plotImages import plotImage
 
 # Load a medical image in tif format
 img = io.imread('../images/jpegImages/1-15.tiff')
@@ -10,8 +11,4 @@ img_eq = exposure.equalize_adapthist(img, clip_limit=0.03)
 # Save the enhanced image
 # io.imsave('../images/jpegImages/1-15-adaptHist.tiff', img_eq)
 
-plt.subplot(121), plt.imshow(img, cmap='gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(img_eq, cmap='gray')
-plt.title('Equalized Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+plotImage(img, img_eq)
