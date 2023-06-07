b07312bdf12ed39f035bc61a8c7f022a344d4a0f
nicole <nicolekozhuharova@gmail.com>
2023-06-07 16:50:09 +0300

    feat: added region growing function and applied it with filters

---
+++ venv/bachelorArbeit/algorithm/functions/comboRegGrow.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import os
+from scipy.ndimage import median_filter
+from skimage import io, exposure
+from PIL import Image, ImageOps, ImageTk, ImageFilter
+from medianFilterFunc import apply_median_filter, apply_sharpen_filter
+from regionGrowingFunc import perform_region_growing
+from calculatePerimeter import calculate_perimeter
+from calculateArea import calculate_area
+
+# Load the image
+originalImage = cv2.imread("../../images/ctisus/ctisusBmp/adrenal_1-01.bmp")
+
+# Apply the median filter
+medianFilteredImage = apply_median_filter(originalImage, 7)
+
+# Apply the sharpen filter
+sharpenFilteredImage = apply_sharpen_filter(medianFilteredImage)
+sharpenFilteredImageArr = np.array(sharpenFilteredImage)
+
+grayImage = cv2.cvtColor(sharpenFilteredImageArr, cv2.COLOR_BGR2GRAY)
+# sharpenFilteredGrayscaleImageArr = np.array(grayImage)
+
+# Apply region growing
+segment = perform_region_growing(grayImage)
+
+# Directory to save the segmented image
+cv2.imwrite('./segmentedImages/regGrowingSegment.bmp', segment)
+cv2.imshow('Region Growing Segment', segment)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Display the results
+cv2.imshow('Original Image', originalImage)
+cv2.imshow('Filtered Image', sharpenFilteredImageArr)
+
+regionOfInterest = './segmentedImages/regGrowingSegment.bmp'
+perimeter = calculate_perimeter(regionOfInterest)
+print('Perimeter:', perimeter)
+
+area = calculate_area(regionOfInterest)
+print('Area:', area)
