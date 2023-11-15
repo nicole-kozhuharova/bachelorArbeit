f6c9a9c7c9eb9fed157385a7036ea91ef9dadca8
[Blubito] Nikol Kozhuharova <nikol.dcqu.kozhuharova@misumi.co.jp>
2023-11-15 13:56:54 +0200

    change file structure

--- venv/bachelorArbeit/convertJpegtoTif.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Open the JPEG image
-jpeg_image = Image.open("./images/ctisus/ctisusJpeg/adrenal_1-07.jpg")
-
-grayscale_image = jpeg_image.convert("L")
-
-# Save the TIFF image
-grayscale_image.save("./images/ctisus/ctisusTiff/adrenal_1-07.tiff")
--- venv/bachelorArbeit/plotImages.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plotImage(originalImage, processedImage):
-    plt.subplot(121), plt.imshow(originalImage, cmap='gray')
-    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(processedImage, cmap='gray')
-    plt.title('Processed Image'), plt.xticks([]), plt.yticks([])
-    plt.show()
